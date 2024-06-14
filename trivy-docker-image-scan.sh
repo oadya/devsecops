c0921169b6367763d7dc3c50b24603a3c648599d
oadya <44276320+oadya@users.noreply.github.com>
2024-06-14 15:30:46 +0200

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -3,7 +3,7 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile) # get docker image
 echo $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName # --light option is to have few details about vulnerability
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
     # Trivy scan result processing
